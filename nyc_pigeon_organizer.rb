45c6fe17b96a42c37dd48814d5c4a6921c55397b
Richard Chen <richard.c.chen@gmail.com>
2018-07-31 23:42:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,33 @@
+require "pry"
 def nyc_pigeon_organizer(data)
   # write your code here!
+  newhash = {}
+  data.each do |att1, att2|
+    att2.each do |att3symbol, birdarray|
+      birdarray.each do |birdname|
+        att3 = att3symbol.to_s
+        #binding.pry
+        if newhash[birdname].nil?
+          newhash[birdname] = {att1 => [att3]}
+          #binding.pry
+        else
+          if newhash[birdname][att1].nil? 
+            newcategory = {att1 => att3}
+            newhash[birdname].merge!(newcategory)
+            #binding.pry
+          else
+            newhash[birdname][att1].push(att3)
+          end
+        end
+      end
+    end
+  end
+binding.pry  
+newhash  
 end
+
+
+
+
+#att1 = :color
+#att3 = :purplel
